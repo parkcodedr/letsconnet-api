85c0429c9c23838f185321d8d576fe9eca6e4c07
ParkCodeDr <33763824+parkcodedr@users.noreply.github.com>
2026-06-17 09:22:12 +0100

    replace url

--- src/cache/redis-cache.module.ts
+++ src/cache/redis-cache.module.ts
@@ -7,7 +7,8 @@ export const REDIS_PUBSUB = 'REDIS_PUBSUB';
 export const REDIS_PUBLISHER = 'REDIS_PUBLISHER';
 
 const createRedisClient = (configService: ConfigService) => {
-  const url = configService.get<string>('REDIS_URL');
+  //const url = configService.get<string>('REDIS_URL');
+  const url = "redis://red-d8ot1fs8aovs73apk01g:6379"
 
   if (!url) {
     throw new Error('REDIS_URL is not defined in environment variables');
