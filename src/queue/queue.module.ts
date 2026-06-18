53f06ae17b89fd5065b4f579eda09d1427ad10de
ParkCodeDr <33763824+parkcodedr@users.noreply.github.com>
2026-06-18 09:59:16 +0100

    fix queue issue on redis connection

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -4,13 +4,21 @@ import { BullModule } from '@nestjs/bullmq';
 @Global()
 @Module({
   imports: [
-    BullModule.forRoot({
-      connection: {
-        host: process.env.REDIS_HOST,
-        port: Number(process.env.REDIS_PORT || 6379),
+    BullModule.forRootAsync({
+      useFactory: () => {
+        const url = process.env.REDIS_URL;
+
+        if (!url) {
+          throw new Error('REDIS_URL is not defined in environment variables');
+        }
+
+        return {
+          connection: {
+            url,
+          },
+        };
       },
     }),
-
     BullModule.registerQueue(
       { name: 'media-processing' },
       { name: 'process-story-media' },
