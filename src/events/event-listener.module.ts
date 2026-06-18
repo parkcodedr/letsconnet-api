c0c39735f745eee7dd81500c11d426763e5353b7
ParkCodeDr <33763824+parkcodedr@users.noreply.github.com>
2026-06-18 21:06:34 +0100

    fix import error

--- src/events/event-listener.module.ts
+++ src/events/event-listener.module.ts
@@ -4,9 +4,10 @@ import { RealtimeModule } from 'src/realtime/realtime.module';
 import { RedisCacheModule } from 'src/cache/redis-cache.module';
 import { PresenceGateway } from 'src/realtime/gateways/presence.gateway';
 import { FriendshipModule } from 'src/friendship/friendship.module';
+import { EventBusModule } from './event-bus.module';
 
 @Module({
-  imports: [RealtimeModule, RedisCacheModule,FriendshipModule],
+  imports: [RealtimeModule, RedisCacheModule,FriendshipModule,EventBusModule],
   providers: [EventListenerService,PresenceGateway],
 })
 export class EventListenerModule {}
